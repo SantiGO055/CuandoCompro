4cd2fb1449fe6202ad9c98ceb98a50774e54fb15
SantiGO055 <santigonzalez05@gmail.com>
2023-01-22 18:03:11 -0300

    first commit

---
+++ src/app/Services/auth.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import {
+  AuthChangeEvent,
+  AuthSession,
+  createClient,
+  Session,
+  SupabaseClient,
+  User,
+} from '@supabase/supabase-js'
+import { environment } from 'src/environments/environment'
+
+export interface Profile {
+  id?: string
+  username: string
+  website: string
+  avatar_url: string
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private supabase: SupabaseClient
+  _session: AuthSession | null = null
+
+  constructor() {
+    this.supabase = createClient(environment.supabaseUrl, environment.supabaseKey)
+  }
+}
